d9387d1fd1c210d08da41adabf30a0e795d709d7
Marcelo Amorim <marceloamorimcg@gmail.com>
2018-08-17 13:39:29 -0300

    Cidade e Estado

--- src/main/java/com/marcelo/cursomc/CursomcApplication.java
+++ src/main/java/com/marcelo/cursomc/CursomcApplication.java
@@ -1,6 +1,7 @@
 package com.marcelo.cursomc;
 
 
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.marcelo.cursomc.domain.Categoria;
+import com.marcelo.cursomc.domain.Cidade;
+import com.marcelo.cursomc.domain.Estado;
 import com.marcelo.cursomc.domain.Produto;
 import com.marcelo.cursomc.repositories.CategoriaRepository;
+import com.marcelo.cursomc.repositories.CidadeRepository;
+import com.marcelo.cursomc.repositories.EstadoRepository;
 import com.marcelo.cursomc.repositories.ProdutoRepository;
 
 
@@ -22,6 +27,11 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -49,6 +59,28 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
+		
+		
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		
+		
+		estadoRepository.save(Arrays.asList(est1, est2));
+		cidadeRepository.save(Arrays.asList(c1, c2, c3));
+		
+		
+		
+		
 		
 		
 		
